34a0328964b4688450e39f68bfef47352d381372
MrNobody98 <morecommands@gmail.com>
2017-03-01 23:52:23 +0100

    release 1.10.2-4.1: Added Chat Channels, improved settings and permissions system, new and many updated commands, ...

--- src/main/java/com/mrnobody/morecommands/command/client/CommandMacro.java
+++ src/main/java/com/mrnobody/morecommands/command/client/CommandMacro.java
@@ -11,10 +11,13 @@ import com.mrnobody.morecommands.command.CommandRequirement;
 import com.mrnobody.morecommands.command.CommandSender;
 import com.mrnobody.morecommands.command.StandardCommand;
 import com.mrnobody.morecommands.core.MoreCommands.ServerType;
+import com.mrnobody.morecommands.network.PacketHandlerClient;
+import com.mrnobody.morecommands.network.PacketHandlerClient.CommandResultCallback;
 import com.mrnobody.morecommands.settings.ClientPlayerSettings;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.client.ClientCommandHandler;
 
 @Command(
@@ -58,12 +61,10 @@ public class CommandMacro extends StandardCommand implements ClientCommandProper
 			else if ((params[0].equalsIgnoreCase("exec") || params[0].equalsIgnoreCase("execute")) && params.length > 1) {
 				List<String> commands = settings.macros.get(params[1]);
 				
-				if (commands != null) {
-					for (String command : commands)
-						if (ClientCommandHandler.instance.executeCommand(Minecraft.getMinecraft().thePlayer, command) == 0)
-							Minecraft.getMinecraft().thePlayer.sendChatMessage(command.startsWith("/") ? command : "/" + command);
-				}
-				else throw new CommandException("command.macro.notFound", sender, params[1]);
+				if (commands != null)
+					new MacroExecutor(sender, commands).continueExecution();
+				else 
+					throw new CommandException("command.macro.notFound", sender, params[1]);
 			}
 			else if ((params[0].equalsIgnoreCase("add") || params[0].equalsIgnoreCase("new") || params[0].equalsIgnoreCase("create") || params[0].equalsIgnoreCase("edit")) && params.length > 2) {
 				if (settings.macros.containsKey(params[1]) && (params[0].equalsIgnoreCase("add") || params[0].equalsIgnoreCase("new") || params[0].equalsIgnoreCase("create")))
@@ -92,5 +93,75 @@ public class CommandMacro extends StandardCommand implements ClientCommandProper
 	@Override
 	public int getDefaultPermissionLevel(String[] args) {
 		return 0;
+	}
+	
+	private static class MacroExecutor implements CommandResultCallback {
+		private int execIdx = 0;
+		private List<String> commands;
+		private CommandSender sender;
+		private String variable;
+		
+		public MacroExecutor(CommandSender sender, List<String> commands) {
+			this.sender = sender;
+			this.commands = commands;
+		}
+		
+		public void continueExecution() {
+			for (; this.execIdx < this.commands.size(); this.execIdx++) {
+				if (this.commands.get(this.execIdx).startsWith("/var grab ") || this.commands.get(this.execIdx).startsWith("var grab")) {
+					grabVarAndBlock(this.commands.get(this.execIdx++));
+					break;
+				}
+				else {
+					String command = this.commands.get(this.execIdx);
+					
+					if (ClientCommandHandler.instance.executeCommand(Minecraft.getMinecraft().thePlayer, command) == 0)
+						Minecraft.getMinecraft().thePlayer.sendChatMessage(command.startsWith("/") ? command : "/" + command);
+				}
+			}
+		}
+		
+		private void grabVarAndBlock(String command) {
+			command = command.substring("var grab ".length() + (command.startsWith("/") ? 1 : 0));
+			this.variable = command.split(" ")[0]; command = command.substring(this.variable.length()).trim();
+			
+			if (!isSenderOfEntityType(sender.getMinecraftISender(), com.mrnobody.morecommands.patch.EntityPlayerSP.class))
+				this.sender.sendLangfileMessage("command.generic.clientPlayerNotPatched", TextFormatting.RED);
+			
+			if (command.startsWith("macro") || command.startsWith("/macro"))
+				this.sender.sendLangfileMessage("command.var.grabMacro", TextFormatting.RED);
+			
+			com.mrnobody.morecommands.patch.EntityPlayerSP patchedPlayer = getSenderAsEntity(this.sender.getMinecraftISender(), com.mrnobody.morecommands.patch.EntityPlayerSP.class);
+			patchedPlayer.setCaptureNextCommandResult();
+			
+			if (ClientCommandHandler.instance.executeCommand(patchedPlayer, command) != 0) {
+				if (patchedPlayer.getCmdSentToServer() != null) {
+    				String cmdSentToServer = patchedPlayer.getCmdSentToServer();
+    				patchedPlayer.getCapturedCommandResult(); //reset
+    				PacketHandlerClient.addPendingRemoteCommand(cmdSentToServer, this);
+				}
+				else setCommandResult(patchedPlayer.getCapturedCommandResult());
+			}
+			else {
+				patchedPlayer.getCapturedCommandResult(); //reset
+				PacketHandlerClient.addPendingRemoteCommand(command, this);
+			}
+		}
+		
+		@Override
+		public void setCommandResult(String result) {
+			com.mrnobody.morecommands.patch.EntityPlayerSP patchedPlayer = getSenderAsEntity(this.sender.getMinecraftISender(), com.mrnobody.morecommands.patch.EntityPlayerSP.class);
+			ClientPlayerSettings settings = getPlayerSettings(patchedPlayer);
+			
+			if (result != null && !result.isEmpty()) settings.variables.put(this.variable, result);
+			this.sender.sendLangfileMessage("command.var.created", variable, result);
+			
+			continueExecution();
+		}
+		
+		@Override
+		public void timeout() {
+			continueExecution();
+		}
 	}
 }
